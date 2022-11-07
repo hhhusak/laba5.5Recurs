9197b079093e7549debf87a581ca7da4cb0a4ce7
hhhusak <bmathhh95@gmail.com>
2022-11-07 18:21:27 +0200

    pushing test to repos

---
+++ src/UnitTest55.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitTest55 {
+
+    @Test
+    void testingMethodQ() {
+        int t;
+        t = Main.Q(5, 5, 1);
+        assertEquals(7, t);
+    }
+}
